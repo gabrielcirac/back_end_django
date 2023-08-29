df998653afca8292fdb9ef70b1d9f5306ec3737c
Gabriel Cirac <gabriel_cirac@yahoo.com.br>
2023-08-29 19:44:14 -0300

    Primeiro Commit

---
+++ funcao.py
@@ -0,0 +1,11 @@
+def print_livro(nome_livro, nome_autor):
+    print(f"Nome do livro: {nome_livro}")
+    print(f"Nome do autor: {nome_autor}")
+
+
+livro = "1984"
+autor = "George Orwel"
+print_livro(livro, autor)
+
+# Faça uma função para calcular o IMC de uma pessoa
+# IMC = peso/(altura^2)
